10728402f44b74ff8905472dde37740e7db2ca2e
Atilla0001 <toulouse663831@gmail.com>
2024-06-22 11:11:10 +0300

    changes

--- app.js
+++ app.js
@@ -1,41 +1,27 @@
 const express = require('express');
 const path = require('path');
 const { engine } = require('express-handlebars');
-
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+
+
+mongoose.connect('mongodb://127.0.0.1/nodeblog_db')
+    .then(() => console.log('MongoDB bağlantısı başarılı'))
+    .catch(err => console.error(err));
+
+
+
+
 
 app.use(express.static("public"));
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
-  res.render("site/index");
-});
-
-
-app.get("/about", (req, res) => {
-  res.render("site/about");
-});
-app.get("/blog", (req, res) => {
-  res.render("site/blog");
-});
-app.get("/contact", (req, res) => {
-  res.render("site/contact");
-});
-app.get("/login", (req, res) => {
-  res.render("site/login");
-});
-app.get("/register", (req, res) => {
-  res.render("site/register");
-});
-
-
-
-app.get("/blog", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "site/blog.html"));
-});
+const main = require('./routes/main')
+app.use('/',main )
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
